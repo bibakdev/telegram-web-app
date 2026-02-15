572238e927197f3de85acbbb26a6642492d4d2e4
Majid <majidbabak@gmail.com>
2026-02-15 21:27:36 +0330

    fix(page)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,6 @@
 import type { Metadata } from 'next';
-import localFont from 'next/font/local';
+import Script from 'next/script';
 import './globals.css';
-
-const vazirmatn = localFont({
-  src: '../node_modules/vazirmatn/Vazirmatn-RD[wght].woff2',
-  variable: '--font-vazirmatn',
-  display: 'swap'
-});
 
 export const metadata: Metadata = {
   title: 'نبرد قبیله‌ها - Group Battle',
@@ -19,13 +13,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html
-      lang="fa"
-      dir="rtl"
-      suppressHydrationWarning
-      className={vazirmatn.variable}
-    >
+    <html lang="fa" dir="rtl" suppressHydrationWarning>
       <head>
+        <link
+          href="https://cdn.jsdelivr.net/gh/rastikerdar/vazirmatn@v33.003/Vazirmatn-font-face.css"
+          rel="stylesheet"
+          type="text/css"
+        />
         <script src="https://telegram.org/js/telegram-web-app.js" async />
       </head>
       <body suppressHydrationWarning>{children}</body>
